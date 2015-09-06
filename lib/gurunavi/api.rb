47033ad02c66955990eb54d92bfd3de6b5c45d6e
sota1235 <sota1235@gmail.com>
2015-09-06 17:18:12 +0900

    initial commit

---
+++ lib/gurunavi/api.rb
@@ -0,0 +1,7 @@
+require "gurunavi/api/version"
+
+module Gurunavi
+  module Api
+    # Your code goes here...
+  end
+end
---
+++ lib/gurunavi/api/version.rb
@@ -0,0 +1,5 @@
+module Gurunavi
+  module Api
+    VERSION = "0.1.0"
+  end
+end
